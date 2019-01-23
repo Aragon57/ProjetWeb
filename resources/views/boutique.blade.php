e9f1734faeac834fdde401f89fa19b0911b2ede6
MrScriptX <dan.liebault@viacesi.fr>
2019-01-23 12:02:15 +0100

    script add cart

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -1,25 +1,27 @@
-<?php session_start(); ?>
+@php 
+  session_start();
+  $_SESSION['id'] = 333;
+@endphp
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Boutique BDE Strasbourg</title>
+    <title>Boutique BDE Strasbourg</title>
 
-  <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/boutique.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('fontawesome/css/style.css') }}" rel="stylesheet">
-  
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/boutique.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('fontawesome/css/style.css') }}" rel="stylesheet">
 
-
-</head>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+  </head>
 
 <body>
 
@@ -127,14 +129,14 @@
 
        <?php 
 
-       foreach($firsts as $first){
+      foreach ($firsts as $first) {
         echo '<div class="col-lg-4 col-md-6 mb-4">
         <div class="card h-100">
 
-        <img src="'.'http://127.0.0.1:8000/' .$first->image .'"   height="60%/9" width="100%/9" class="" >
+        <img src="' . 'http://127.0.0.1:8000/' . $first->image . '"   height="60%/9" width="100%/9" class="" >
 
         <div class="">
-        <h4 class="card-title">' .$first->name. '</h4> 
+        <h4 class="card-title">' . $first->name . '</h4> 
         <h5> Prix : ' . $first->price . '€</h5>
         <p class="card-text">' . $first->description . '</p>
         <button class="addtocart-btn" type="submit"> Ajouter au panier <span> </span> <i class="fas fa-shopping-cart"> </i></button>
@@ -203,51 +205,45 @@
 
 
             <div class="row">
+              
+              @foreach ($products as $product)
+                @php 
+                  $cate = $product['category'];
 
-              <?php 
+                  $caract_interdit = array(
+                    'Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
+                    'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U',
+                    'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c',
+                    'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o',
+                    'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y', ' ' => ''
+                  );
 
-              foreach($products as $product){
-
-                
-
-                $cate = $product['category'];
-
-     $caract_interdit = array( 
-                'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
-                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
-                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
-                'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
-                'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', ' '=>'' );
-
-                $cate = strtr( $cate, $caract_interdit );
-                $cate = strtolower($cate);
-
-                echo '<div class= "col-lg-4 col-md-6 mb-4 article '. $cate.'">
-                <div class="card h-100">
-
-                <img src="'.'http://127.0.0.1:8000/' .$product->image .'"   height="60%/9" width="100%/9" class="" >
-
-                <div class="">
-                <h4 class="card-title">' .$product->name. '</h4> 
-                <h5> Prix : ' . $product->price . '€</h5>
-                <p class="card-text">' . $product->description . '</p>
-                <button class="addtocart-btn" type="submit"> Ajouter au panier <span> </span> <i class="fas fa-shopping-cart"> </i></button>
-
-                </div>
-
-                </div>
-                </div>';
-              }
-             
-              ?>
+                  $cate = strtr($cate, $caract_interdit);
+                  $cate = strtolower($cate);
+                @endphp
+                  <form method="POST">
+                  @csrf
+                  <input type="hidden" name="id" value={{ $_SESSION['id'] }}>
+                  <div class= "col-lg-4 col-md-6 mb-4 article  ">
+                    <div class="card h-100">
+                      <img src="http://127.0.0.1:8000/{{ $product->image }}"   height="60%/9" width="100%/9" class="" >
+                      <div class="">
+                        <h4 class="card-title"> {{ $product->name }}</h4> 
+                        <h5> Prix :{{ $product->price }}€</h5>
+                        <p class="card-text">{{ $product->description }}</p>
+                        <button class="addtocart-btn" type="submit"> Ajouter au panier <span> </span> <i class="fas fa-shopping-cart"> </i></button>
+                      </div>
+                    </div>
+                  </div>
+              </form>
+              @endforeach
 
             
             </div>
           </div>
 
 
-          <div class="col-lg-1 col-md-2 col-sm-3 bg-dark">
-          </div>
+          <div class="col-lg-1 col-md-2 col-sm-3 bg-dark"></div>
 
 
       
@@ -264,13 +260,13 @@
     </body>
 
   </body>
-  <?php $_SESSION['id'] = 333; ?>
+  
   <script>
     $(document).ready(()=>{
       $('.addtocart-btn').click(()=>{
           console.log("here");
           var form = {
-            'id_user' : "<?php echo $_SESSION['id']?>",
+            'id_user' : "<?php echo $_SESSION['id'] ?>",
             'id_product' : $('input[name=id]').val(),
             'id_command' : 0,
             'quantity' : 1
