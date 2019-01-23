1b8090e5909c65d4c03e546327d331f38485633d
Killianhtz <32952424+Killianhtz@users.noreply.github.com>
2019-01-23 10:49:54 +0100

    filter products by category

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Boutique BDE Strasbourg</title>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Boutique BDE Strasbourg</title>
+
+  <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/boutique.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('fontawesome/css/style.css') }}" rel="stylesheet">
   
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/boutique.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('fontawesome/css/style.css') }}" rel="stylesheet">
+
+
 </head>
 
 <body>
+
+
  <!-- Nav bar -->
 
 
@@ -117,14 +124,14 @@
 
        <?php 
 
-      foreach ($firsts as $first) {
+       foreach($firsts as $first){
         echo '<div class="col-lg-4 col-md-6 mb-4">
         <div class="card h-100">
 
-        <img src="' . 'http://127.0.0.1:8000/' . $first->image . '"   height="60%/9" width="100%/9" class="" >
+        <img src="'.'http://127.0.0.1:8000/' .$first->image .'"   height="60%/9" width="100%/9" class="" >
 
         <div class="">
-        <h4 class="card-title">' . $first->name . '</h4> 
+        <h4 class="card-title">' .$first->name. '</h4> 
         <h5> Prix : ' . $first->price . '€</h5>
         <p class="card-text">' . $first->description . '</p>
         <button class="addtocart-btn" type="submit"> Ajouter au panier <span> </span> <i class="fas fa-shopping-cart"> </i></button>
@@ -173,14 +180,13 @@
       <div class="recherche_p">
         <div id="filteredproducts" align="left">
 
-         <ul class="dropdownmenu">
-          
-          <li><a href="">Prix</a>
-            <li><a href="#">Nom</a>
-              <li><a href="#">Vêtements</a>
-                <li><a href="#">Accessoires</a>
-                  <li><a href="#">Goodies</a>
-                  </ul>
+        <ul class="dropdownmenu">
+            <li><button class="btn btn-light tri" data="materielinformatique"> Prix </button></li>
+            <li><button class="btn btn-light tri" data="1"> Vêtements </button></li>
+            <li ><button class="btn btn-light tri" data="2"> Accessoires </button></li>
+            <li><button class="btn btn-light tri" data="3"> Goodies </button></li>
+        </ul >
+
                 </div>
               </div>
             </div>
@@ -193,16 +199,30 @@
             <br>
 
             <div class="row">
+
               <?php 
 
-              foreach ($products as $product) {
-                echo '<div class="col-lg-4 col-md-6 mb-4">
+              foreach($products as $product){
+
+                $cate = $product['category'];
+
+     $caract_interdit = array( 
+                'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+                'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+                'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', ' '=>'' );
+
+                $cate = strtr( $cate, $caract_interdit );
+                $cate = strtolower($cate);
+
+                echo '<div class= "col-lg-4 col-md-6 mb-4 article '. $cate.'">
                 <div class="card h-100">
-                <input type="hidden" class="id" name="id" value="'.$product->id.'">
-                <img src="' . 'http://127.0.0.1:8000/' . $product->image . '"   height="60%/9" width="100%/9" class="" >
+
+                <img src="'.'http://127.0.0.1:8000/' .$product->image .'"   height="60%/9" width="100%/9" class="" >
 
                 <div class="">
-                <h4 class="card-title">' . $product->name . '</h4> 
+                <h4 class="card-title">' .$product->name. '</h4> 
                 <h5> Prix : ' . $product->price . '€</h5>
                 <p class="card-text">' . $product->description . '</p>
                 <button class="addtocart-btn" type="submit"> Ajouter au panier <span> </span> <i class="fas fa-shopping-cart"> </i></button>
@@ -210,8 +230,7 @@
                 </div>
 
                 </div>
-                </div>
-                ';
+                </div>';
               }
               ?>
 
@@ -233,20 +252,11 @@
       <script src="{{ asset('js/bootstrap.min.js') }}"></script>
       <script src="{{ asset('js/jquery.min.js') }}"></script>
       <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+      <script src="{{ asset('/js/boutique.js') }}"></script>
 
 
-  </body>
-  <script>
-    $(document).ready(()=>{
-      $('addtocart-btn').click(()=>{
-        let send_data = $('input[name=id]');
-        $.post('', send_data, (data, status)=>{
-          console.log(data);
-          console.log(status);
-        });
-      });
-    });
-  </script>  
-</html>
+    </body>
+
+    </html>
 
 
