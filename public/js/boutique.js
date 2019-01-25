b892198dab6d276be837f4d42d6885031e210e0f
MrScriptX <dan.liebault@viacesi.fr>
2019-01-25 14:30:47 +0100

    style change and code format

--- public/js/boutique.js
+++ public/js/boutique.js
@@ -1,32 +1,33 @@
-  $(function() {
-  
-      $('.tri').click (function() {
-        var element_clique = $(this);
+$(function () {
 
-        //Si clic sur l'element alors qu'il est déjà actif => on réaffiche tous les articles
-        if(element_clique.hasClass('active-tri')) {
-          element_clique.removeClass('active-tri');
-          $('.article').show();
-          return;
-        }
+	$('.tri').click(function () {
+		var element_clique = $(this);
 
-        //Si clic sur un bouton mais qu'un autre tri est déjà actif on l'annule
-        if($('.tri').hasClass('active-tri')) {
-          $('.tri').removeClass('active-tri');
-          $('.article').show();
-        }
+		//Si clic sur l'element alors qu'il est déjà actif => on réaffiche tous les articles
+		if (element_clique.hasClass('active-tri')) {
+			element_clique.removeClass('active-tri');
+			$('.article').show();
+			return;
+		}
 
-        //Si clic sur un bouton mais qu'un autre tri est déjà actif on l'annule     
-        if(element_clique.hasClass('active-tri')) {
-          element_clique.removeClass('active-tri');
-          $('.article').show();
-        }else {
-          var categorie = element_clique.attr("value");
-          element_clique.addClass('active-tri');
-          $('.article').not('.' + categorie).hide();
-        }
+		//Si clic sur un bouton mais qu'un autre tri est déjà actif on l'annule
+		if ($('.tri').hasClass('active-tri')) {
+			$('.tri').removeClass('active-tri');
+			$('.article').show();
+		}
 
-      });
+		//Si clic sur un bouton mais qu'un autre tri est déjà actif on l'annule     
+		if (element_clique.hasClass('active-tri')) {
+			element_clique.removeClass('active-tri');
+			$('.article').show();
+		} else {
+			console.log('here');
+			var categorie = element_clique.attr("value");
+			element_clique.addClass('active-tri');
+			$('.article').not('.' + categorie).hide();
+		}
+
+	});
 
 });
 
--- routes/web.php
+++ routes/web.php
@@ -17,61 +17,100 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/boutique', 'ProductController@display' ,function () {
-    return view('boutique');
+Route::get('/inscription', function () {
+    return view('inscription');
 });
 
 Route::get('/connexion', function () {
     return view('connexion');
 });
 
-
-Route::get('/idee',  'IdeeController@display',  function () {
-    return view('idee');
+Route::get('/event' , 'EventController@display', function () {
+    return view('event');
 });
 
-Route::get('/inscription', function () {
-    return view('inscription');
+Route::get('/boutique', 'ProductController@display', function () {
+    return view('boutique');
 });
 
 Route::get('/cart', function () {
     return view('cart');
 });
 
+Route::get('/politique', function () {
+    return view('politique');
+});
 
-Route::get('/event' , 'EventController@display', function () {
-    return view('event');
+Route::get('/mentions', function () {
+    return view('mentions');
+});
+
+Route::get('/conditions', function () {
+    return view('conditions');
+});
+
+Route::get('/credits', function () {
+    return view('credits');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/cart', 'CommandController@show', function() {
+    return view('cart');
+});
+
+Route::get('/footer', function () {
+    return view('footer');
+});
+
+Route::get('/idee',  'IdeeController@display',  function () {
+    return view('idee');
+});
+
+Route::get('/cartview', function () {
+    return view('layouts/cartview');
+});
+
+Route::get('/articlenon', function () {
+    return view('layouts/articlenon');
 });
 
 Route::get('/usernav', function () {
     return view('layouts/usernav');
 });
 
-
 Route::get('/article', function () {
     return view('layouts/article');
 });
 
-Route::get('/articlenon', function () {
-    return view('layouts/articlenon');
-
-Route::get('/cartview', function () {
-    return view('layouts/cartview');
+Route::get('/modal', function () {
+    return view('modal');
 });
 
-Route::post('/image', 'ImageController@store');
+Route::get('/evenementdumois', function () {
+    return view('evenementdumois');
+});
 
-Route::post('/image', 'EventController@storeImage');
+Route::get('/shopcomp', function () {
+    return view('components/shopcomp');
+});
 
-
+Route::delete('/product', 'CommandController@deleteproduct');
+Route::post('/product', 'ProductController@getProduct');
+Route::put('/product', 'CommandController@updatequantity');
+Route::post('/showcart', 'CommandController@show');
+Route::get('/islogged', 'UsersController@isLogged');
+Route::get('/fetchcart', 'CartController@cart');
+Route::post('/article', 'ProductController@addarticle');
+Route::post('/tocart', 'CommandController@addarticle');
+Route::post('/product/destroy', 'ProductController@destroy');
 Route::post('/comment', 'EventController@storeComment');
 Route::post('/commentImage', 'EventController@storeCommentImage');
 Route::post('/event/downloadPic', 'EventController@downloadPic');
-
 Route::post('/like', 'LikeController@store');
-
 Route::post('/validate', 'IdeeController@update');
-
 Route::post('/inscription', 'UsersController@store');
 Route::post('/connexion', 'UsersController@connect');
 Route::get('/logout', 'UsersController@logout');
@@ -79,57 +118,8 @@ Route::post('/idee', 'IdeeController@store');
 Route::put('/idee/{id}', 'IdeeController@update');
 Route::post('/event', 'EventController@store');
 Route::post('/image/delete', 'EventController@deleteImage');
-
 Route::post('/comment/delete', 'EventController@deleteComment');
-
-
 Route::post('/category', 'CategoryController@store');
-
 Route::post('/inscription', 'UsersController@store');
-
-Route::get('/footer', function () {
-    return view('footer');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
-Route::get('/evenementdumois', function () {
-    return view('evenementdumois');
-});
-Route::post('/product/destroy', 'ProductController@destroy');
-
-Route::get('/cart', 'CommandController@show', function() {
-    return view('cart');
-});
-
-Route::get('/islogged', 'UsersController@isLogged');
-
-Route::get('/fetchcart', 'CartController@cart');
-
-Route::post('/article', 'ProductController@addarticle');
-
-Route::post('/tocart', 'CommandController@addarticle');
-
-Route::post('/showcart', 'CommandController@show');
-Route::delete('/product', 'CommandController@deleteproduct');
-Route::post('/product', 'ProductController@getProduct');
-Route::put('/product', 'CommandController@updatequantity');
-
-
-Route::get('/politique', function () {
-    return view('politique');
-});
-Route::get('/mentions', function () {
-    return view('mentions');
-});
-Route::get('/conditions', function () {
-    return view('conditions');
-});
-Route::get('/credits', function () {
-    return view('credits');
-});
-Route::get('/modal', function () {
-    return view('modal');
-});
-
-
+Route::post('/image', 'ImageController@store');
+Route::post('/image', 'EventController@storeImage');
