76bdfb258a68176f62c9197191c2f3717e6443e7
Killianhtz <32952424+Killianhtz@users.noreply.github.com>
2019-01-29 11:55:24 +0100

    commentaire

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -137,46 +137,37 @@ class CommandController extends Controller
         }
 
 
-
+        //Gestion et envoi des mails
         date_default_timezone_set('Etc/UTC');
         require '../vendor/autoload.php';
-            //Create a new PHPMailer instance
+            
         $mail = new PHPMailer;
-            //Tell PHPMailer to use SMTP
+            
         $mail->isSMTP();
-            //Enable SMTP debugging
-            // 0 = off (for production use)
-            // 1 = client messages
-            // 2 = client and server messages
+            
         $mail->SMTPDebug = 2;
-            //Ask for HTML-friendly debug output
+       
         $mail->Debugoutput = 'html';
-            //Set the hostname of the mail server
+          
         $mail->Host = 'smtp.gmail.com';
-            // use
-            // $mail->Host = gethostbyname('smtp.gmail.com');
-            // if your network does not support SMTP over IPv6
-            //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+
         $mail->Port = 587;
-            //Set the encryption system to use - ssl (deprecated) or tls
+          
         $mail->SMTPSecure = 'tls';
-            //Whether to use SMTP authentication
+       
         $mail->SMTPAuth = true;
-            //Username to use for SMTP authentication - use full email address for gmail
+        
         $mail->Username = "noreply.bde@gmail.com";
-            //Password to use for SMTP authentication
+       
         $mail->Password = "Jesuiskillian";
-            //Set who the message is to be sent from
+           
         $mail->setFrom('noreply.bde@gmail.com', 'BDE');
-            //Set an alternative reply-to address
-            //Set who the message is to be sent to
+           
         $mail->addAddress($_SESSION['email'], 'BDE');
 
-            //Set the subject line
+            
         $mail->Subject = 'Validation de votre commande';
-            //Read an HTML message body from an external file, convert referenced images to embedded,
-            //convert HTML into a basic plain-text alternative body
-            //Replace the plain text body with one created manually
+           
         $mail->Body = 'Votre commande est valide, nous vous attendons pour proceder a l\'echange';
         $mail->SMTPOptions = array(
             'ssl' => array(
@@ -185,8 +176,7 @@ class CommandController extends Controller
               'allow_self_signed' => true
           )
         );
-            //Attach an image file
-            //send the message, check for errors
+    
         if (!$mail->send()) {
             echo "Mailer Error: " . $mail->ErrorInfo;
         } else {
